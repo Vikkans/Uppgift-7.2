a31d9e8d80df4a22776c71cde022eca71c56bf8b
Vikkans <143171920+Vikkans@users.noreply.github.com>
2024-02-09 12:34:20 +0100

    Add project files.

---
+++ ConsoleApp59/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace COnsole
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> maxtal = new List<int>();
+            maxtal.Add(5);
+            maxtal.Add(6);
+            maxtal.Add(33);
+            Max(maxtal);
+           
+            
+        }
+
+        static void Max(List<int> maxtal)
+        {
+           
+            
+            int störstatal = 0;
+
+            for (int i = 0; i < maxtal.Count; i++)
+            {
+                if (maxtal[i] > störstatal) 
+                {
+                   störstatal = maxtal[i];
+                }
+            }
+
+            Console.WriteLine(störstatal);
+        
+        }
+     
+           
+            
+         
+       
+    }
+}
